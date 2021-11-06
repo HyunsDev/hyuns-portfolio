7d7ffb635eb4a961552958d8e68171d82576fd08
혀느현스 <hyuns@hyuns.dev>
2021-11-07 01:35:34 +0900

    CRA 생성 및 인트로 제작 중

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# hyuns-portfolio
+ 혀느현스 포트폴리오 웹사이트
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LoadingScreen from "./screen/loading"
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <div className="App">
+      <LoadingScreen />
+    </div>
+  );
+}
+
+export default App;
